ee22ec27c8d2dcad8c5ef4e195df6f3f67575f04
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2017-10-28 21:24:34 +0200

    User profile show

--- app/components/users/show-profile/component.js
+++ app/components/users/show-profile/component.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service'
 
 export default Component.extend({
+  session: service(),
+
+  classNames: ['user-profile'],
+
+  actions: {
+    saveProfile() {
+      this.get('user.save');
+    }
+  }
 });
