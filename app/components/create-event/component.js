ea32641115e8bd341a3c50be39cc4fc4c900de2d
BobrImperator <lordboberro@gmail.com>
2017-10-28 18:16:05 +0200

    event creation component

--- app/components/create-event/component.js
+++ app/components/create-event/component.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
 import {inject as service} from '@ember/service';
+
 export default Component.extend({
+  store: service(),
+
+  actions:{
+    newEvent(){
+      let store = this.get('store'),
+          title = this.get('title'),
+          description = this.get('description'),
+          startDate = this.get('startDate'),
+          duration = this.get('duration');
+      let event = store.createRecord('event',{
+        title: title,
+        description: description,
+        startDate: startDate,
+        duration: duration,
+      });
+      event.save();
+    }
+  }
 });
