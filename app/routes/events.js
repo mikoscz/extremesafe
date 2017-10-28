8772ac22a1d52407cbfbd30bbae365e923c98fbb
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2017-10-28 21:43:27 +0200

    Implement events listing

--- app/routes/events.js
+++ app/routes/events.js
@@ -1,15 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
   model(){
     return this.get('store').findAll('event');
   },
 
   actions: {
     newEvent(params){
-    let event =  this.store.createRecord('event', params);
-    event.save();
+      let event =  this.store.createRecord('event', params);
+      event.save();
     }
   }
 });
