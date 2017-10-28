cc44b499ca7c5b04c4fbe88d00761196e94f206a
BobrImperator <lordboberro@gmail.com>
2017-10-28 18:15:25 +0200

    Generate model event and add attributes

--- app/models/event.js
+++ app/models/event.js
@@ -1,17 +1,9 @@
 import DS from 'ember-data';
-import {computed} from 'emberObject';
 export default DS.Model.extend({
-  name: DS.attr('string'),
+  title: DS.attr('string'),
   description: DS.attr('string'),
 
-  startDate: DS.attr('date'),
-  endDate: DS.attr('date'),
-
-  duration: computed('startDate', 'endDate',function(){
-    let startDate = this.get('startDate'),
-        endDate = this.get('endDate');
-    let diffBetween = endDate - startDate;
-    return Math.ceil(diffBetween / (1000));
-  }),
+  startDate: DS.attr(),
+  duration: DS.attr('string'),
 
 });
