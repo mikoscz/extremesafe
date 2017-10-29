b085b4b399c482493e5d876123026203f62bf995
BobrImperator <lordboberro@gmail.com>
2017-10-29 06:03:38 +0100

    User can save in his profile "preferences"

--- app/models/user.js
+++ app/models/user.js
@@ -4,4 +4,11 @@ export default DS.Model.extend({
   uid: DS.attr('string'),
   avatarUrl: DS.attr('string'),
   name: DS.attr('string'),
+
+  individual: DS.attr('boolean'),
+  wheelchair: DS.attr('boolean'),
+  spineInjury: DS.attr('boolean'),
+  amputation: DS.attr('boolean'),
+  cerebralPalsy: DS.attr('boolean')
+
 });
